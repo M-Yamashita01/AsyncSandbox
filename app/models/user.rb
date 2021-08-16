8c0443c97462b8abfba200146f4e4691151ee6e4
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-08-17 08:12:47 +0900

    Create send_delay_message method to execute the method by rails console.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   def send_message
     puts '----send_message---'
   end
+
+  def self.send_delay_message
+    user = new
+    user.save!
+    Delayed::Worker.lifecycle.before(:enqueue) do
+      puts '---before_action---'
+    end
+    user.delay(priority: 10, run_at: Time.now + 10).send_message 
+  end
 end
